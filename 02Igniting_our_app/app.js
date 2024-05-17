60d3d7a9bc1d5f5a05a291c2913bf065fefa3e4f
rajat jana <rajatjana2024@gmail.com>
2024-05-17 19:58:58 +0530

    02Ignating our app completed

---
+++ 02Igniting_our_app/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const heading1 = React.createElement('h1', {
+     class:"heading",
+}, 'Headin1 React!');
+const heading2 = React.createElement('h5', {
+     class:"heading",
+}, 'Headin2 React!');
+
+const container = React.createElement('div', {
+     id:"container",
+}, [heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(container);
